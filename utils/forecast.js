7de77581a1f7329ce4f07bbcb20352d2cb1dd9d9
Ankur Tripathi <ankur.2323@gmail.com>
2020-02-10 22:35:32 +0530

    refactored code

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,17 +1,17 @@
 const request = require('request');
 
 
-const forecast = (data, callback) => {
-    const url = `https://api.darksky.net/forecast/d2f421c1ff097bb771aeb1032c04cb77/${data.location}`;
-    request({ url: url, json: true},(error,response,body) => {
+const forecast = ({location, placeName}, callback) => {
+    const url = `https://api.darksky.net/forecast/d2f421c1ff097bb771aeb1032c04cb77/${location}`;
+    request({ url, json: true},(error,response,body) => {
         if(error) {
             callback('Unable to fetch weather from API');
         }
-        else if(response.body.code === 400) {
+        else if(body.code === 400) {
             callback(body.error);
         }
         else {
-            callback(undefined, ` In ${data.placeName} it is ${body.daily.data[0].summary}It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain today. `)
+            callback(undefined, ` In ${placeName} it is ${body.daily.data[0].summary}It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain today. `)
         }
     });
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiYW5rdXIxOTA5IiwiYSI6ImNrNmVsdmg4YjBmYjIza3FwemNtYzE0MWkifQ._ZdbPjUEIoogr030OhZbKg`
-    request({url:url, json:true}, (error,response,body) => {
+    request({url, json:true}, (error,response,body) => {
         if(error) {
             callback('Unable to connect to geocoding service.');
         }
